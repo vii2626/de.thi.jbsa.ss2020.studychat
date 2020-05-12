c1086a796cce67a98c26e03d7c1ec9a19e4a013a
Vincent PC <vii2626@thi.de>
2020-05-12 14:38:11 +0200

    Neue Methode zum holen der Daten aus der Event Datenbank

--- b-domain-handler/src/main/java/de/thi/jbsa/prototype/repository/EventRepository.java
+++ b-domain-handler/src/main/java/de/thi/jbsa/prototype/repository/EventRepository.java
@@ -1,12 +1,21 @@
 package de.thi.jbsa.prototype.repository;
 
+import de.thi.jbsa.prototype.model.EventName;
+//import de.thi.jbsa.prototype.model.event.AbstractEvent;
 import org.springframework.data.repository.CrudRepository;
 import de.thi.jbsa.prototype.model.EventEntity;
+
+//import java.util.UUID;
 
 /**
  * @author Christopher Timm <christopher.timm@beskgroup.com> on 27.02.18
  */
 public interface EventRepository
-  extends CrudRepository<EventEntity, Long> {
+        extends CrudRepository<EventEntity, Long> {
 
+//  EventEntity findByUuid(UUID uuid);
+
+//  EventEntity findFirstByEventNameOrderByIdDesc(EventName eventname);
+
+  EventEntity findFirstByEventNameAndValueContainingOrderByIdDesc(EventName eventname, String containedInValue);
 }
